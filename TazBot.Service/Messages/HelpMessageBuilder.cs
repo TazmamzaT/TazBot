67a341f8a5f1625164b957b7d90a548b2e172f69
TazmamzaT <tazmamzat@gmail.com>
2021-08-29 17:44:28 -0500

    Add support for dynamic help message generation

--- TazBot.Service/Messages/HelpMessageBuilder.cs
+++ TazBot.Service/Messages/HelpMessageBuilder.cs
@@ -1,4 +1,5 @@
 ﻿using Discord;
+using Discord.Commands;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,23 +12,67 @@ namespace TazBot.Service.Messages
     {
         public HelpMessageBuilder()
         {
-            Fields = new List<EmbedFieldBuilder>()
-            {
-                new(){ Name = "!taz", Value = "joke"},
-                new(){ Name = "!taz", Value = "thanks"},
-                new(){ Name = "!taz", Value = "insult [@user]"},
-                new(){ Name = "!taz", Value = "spongebob-gif"},
-                new(){ Name = "!taz reddit", Value = "dailytop3 [subreddit]"},
-                new(){ Name = "!taz reddit", Value = "randomtop [subreddit]"},
-                new(){ Name = "!taz reddit", Value = "randomhot [subreddit]"},
-                new(){ Name = "!taz google", Value = "luckyimage [query (quotes)]"},
-                new(){ Name = "!taz gif", Value = "random [tag]"},
-            };
-
             Title = "Command List";
             ThumbnailUrl = "https://avatars2.githubusercontent.com/u/25140729?s=460&u=e87cfa6adaed41b3b9f1d77c8d45165989233160&v=4";
 
             Color = Discord.Color.Red;
+
+            GenerateHelpUsingReflection();
+        }
+
+        private void GenerateHelpUsingReflection()
+        {
+            Fields = new List<EmbedFieldBuilder>();
+
+            var commandmodules = AppDomain.CurrentDomain.GetAssemblies()
+                .SelectMany(s => s.GetTypes())
+                .Where(p => typeof(ModuleBase<SocketCommandContext>).IsAssignableFrom(p))
+                .ToList();
+
+            foreach(var module in commandmodules)
+            {
+                // Should only be one group per module
+                GroupAttribute group = (GroupAttribute) Attribute.GetCustomAttribute(module, typeof(GroupAttribute));
+                //CommandAttribute[] commands = (CommandAttribute[]) Attribute.GetCustomAttributes(module, typeof(CommandAttribute));
+                var methods = module.GetMethods();
+                //var commands2 = methods.Where(m => m.CustomAttributes.Any(ca => ca.GetType() == typeof(CommandAttribute))).ToList();
+                var commands = methods.Where(m => m.CustomAttributes.Any(ca => ca.AttributeType == typeof(CommandAttribute))).ToList();
+
+                foreach (var command in commands)
+                {
+                    //Fields.Insert(0, new() { Name = "!taz", Value = command.CustomAttributes.Where(a => a.AttributeType == typeof(CommandAttribute)).First().ConstructorArguments.First().Value.ToString() });
+                    var attributes = (CommandAttribute[])Attribute.GetCustomAttributes(command, typeof(CommandAttribute));
+                    //Fields.Insert(0, new() { Name = "!taz", Value = command.CustomAttributes.Where(a => a.AttributeType == typeof(CommandAttribute)).First().ConstructorArguments.First().Value.ToString() });
+                    var temp = command.GetParameters();
+                    foreach (var attribute in attributes)
+                    {
+                        string groupname = string.Empty;
+                        if (group is not null)
+                        {
+                            groupname = $" {group.Prefix}";
+                        }
+
+                        if (groupname == string.Empty)
+                        {
+                            var text = attribute.Text;
+                            foreach (var param in temp)
+                            {
+                                text += $" [{param.Name}]";
+                            }
+                            Fields.Insert(0, new EmbedFieldBuilder { Name = $"!taz{groupname}", Value = text });
+                        }
+                        else
+                        {
+                            var text = attribute.Text;
+                            foreach (var param in temp)
+                            {
+                                text += $" [{param.Name}]";
+                            }
+                            Fields.Add(new EmbedFieldBuilder { Name = $"!taz{groupname}", Value = text });
+                        }
+                    }
+                }
+            }
         }
     }
 }
